d6f95f5290f81cb7460c08f0b9f42a275c1e1345
Tenpi <37512637+Tenpi@users.noreply.github.com>
2019-10-20 02:31:38 -0400

    fix

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import Pixiv from "./Pixiv"
 require("dotenv").config();
 (async () => {
     const pixiv = await Pixiv.login(process.env.PIXIV_USERNAME, process.env.PIXIV_PASSWORD)
+    pixiv
     // const result = await pixiv.illust.get("https://www.pixiv.net/en/artworks/77240733")
     /*await pixiv.util.downloadIllusts("black tights 00", "./downloads", "large", [{
         folder: "stockings 2", tag: "black tights"
     }])*/
-    await pixiv.util.downloadUgoira("https://www.pixiv.net/en/artworks/77382629", "./downloads", "webp", 50)
+    // await pixiv.util.downloadUgoira("https://www.pixiv.net/en/artworks/77382629", "./downloads", "webp", 50)
 })()
