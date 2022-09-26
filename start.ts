a328e147951d8b06109a68fc594918173c3e2270
Tenpi <pie4chan@gmail.com>
2022-09-26 15:44:21 -0400

    test

--- start.ts
+++ start.ts
@@ -7,16 +7,11 @@ import Pixiv from "./pixiv"
 require("dotenv").config();
 (async () => {
     const pixiv = await Pixiv.refreshLogin(process.env.REFRESH_TOKEN)
-    let illusts = await pixiv.search.illusts({word: "loli"})
+    let illusts = await pixiv.search.illusts({word: "cute"})
     await pixiv.util.downloadIllust(illusts[0], "./downloads", "original")
     const files = fs.readdirSync(path.join(__dirname, "../downloads")).map((p) =>{
         return path.join(__dirname, "../downloads", p)
     })
     const hash = crypto.createHash("md5").update(fs.readFileSync(files[0])).digest("hex")
     console.log(hash)
-    setTimeout(() => {
-        const hash2 = crypto.createHash("md5").update(fs.readFileSync(files[0])).digest("hex")
-        console.log(hash)
-        console.log(hash === hash2)
-    }, 5000)
 })()
