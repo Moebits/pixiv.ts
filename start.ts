7c6281726a4a6add2ccc0f0fa3a1bc67c5d5ca15
Tenpi <pie4chan@gmail.com>
2024-09-01 22:29:47 -0400

    version

--- start.ts
+++ start.ts
@@ -6,7 +6,7 @@ import Pixiv from "./pixiv"
 
 require("dotenv").config();
 (async () => {
-    const pixiv = await Pixiv.refreshLogin(process.env.REFRESH_TOKEN)
+    const pixiv = await Pixiv.refreshLogin(process.env.PIXIV_REFRESH_TOKEN)
     const result = await pixiv.illust.comments({illust_id: 72301885})
     console.log(result.comments[0])
 })()
--- test/login.ts
+++ test/login.ts
@@ -5,7 +5,7 @@ let pixiv: Pixiv
 
 export default async () => {
     if (!pixiv) {
-        pixiv = await Pixiv.refreshLogin(process.env.REFRESH_TOKEN)
+        pixiv = await Pixiv.refreshLogin(process.env.PIXIV_REFRESH_TOKEN)
     }
 }
 
