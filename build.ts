ad9aa69dc9d3a0017da9e3069c3e8bb56e180616
Tenpi <pie4chan@gmail.com>
2025-10-23 02:24:45 -0400

    browser build?

---
+++ build.ts
@@ -0,0 +1,22 @@
+import {build} from "esbuild"
+import {NodeModulesPolyfillPlugin} from "@esbuild-plugins/node-modules-polyfill"
+import {NodeGlobalsPolyfillPlugin} from "@esbuild-plugins/node-globals-polyfill"
+
+build({
+  entryPoints: ["dist/pixiv.js"],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  platform: "browser",
+  format: "esm",
+  outfile: "dist/pixiv.browser.js",
+  plugins: [
+    NodeGlobalsPolyfillPlugin({
+      process: true,
+      buffer: true,
+    }),
+    NodeModulesPolyfillPlugin()
+  ],
+  mainFields: ["browser", "module", "main"],
+  external: ["node:fs/promises", "http2", "fs", "path", "crypto", "@aws-sdk/client-s3"],
+})
